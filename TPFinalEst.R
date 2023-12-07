dc3f270a6862715252ffe53f112610778aac6664
lucasGalliCasado <lucasgallicasado@gmail.com>
2023-12-07 12:19:42 -0300

    Comienzo de Ej1, bootstrap

---
+++ TPFinalEst.R
@@ -0,0 +1,87 @@
+TrainTest <- read.table("TrainTest.txt")
+
+body <- read.csv("body.csv")
+
+#Le asignamos el nombre correspondiente a las columnas del data set
+colnames(body)<- (c("BIAC","BIIL","BITRO","CHEST1","CHEST2","ELBOW","WRIST",
+                    "KNEE","ANKLE","SHOUL","CHESTG","WAISTG","NAVEL","HIP","GLUTE","BICEP",
+                    "FLOREA","KNEEG","CALF","ANKLEG","WRISTG","AGE","WEIG","HEIG","GEN"))
+
+#Lectura de Datos -----------------------------------------------------------------------------------------------------------
+ 
+#Nos fijamos si hay datos NULL
+any(is.na(body))
+
+#Nos Fijamos el tipo de dato de cada columna
+sapply(body, class)
+
+# Conclusion: No hay datos nulos y los tipos de dato corresponden a lo pedido
+
+#Etapa Exploratoria -----------------------------------------------------------------------------------------------------------
+
+#b.)
+
+#Primero separamos los datos segun su gener0
+
+#Weight Men y su medianda
+WEIG_M <-  body[body$GEN == 1, ]$WEIG
+
+T_M <- median(WEIG_M)
+
+
+#Weight Women y su mediana 
+WEIG_W <-  body[body$GEN == 0, ]$WEIG
+
+T_W <- median(WEIG_W)
+
+#BootStrap para pesos de hombres
+B = 1000
+
+# Tomamos 1000 muestras de tamano |WEIGH_M| de los datos que tenemos 
+n <- length(WEIG_M)
+estMedMen<- 0
+
+for(i in 1:B){
+  #Para cada muestra estimamos la mediana y la almacenamos 
+  muestra <- sample(WEIG_M, n, replace = TRUE )
+  estMedMen[i] <- median(muestra)
+}
+
+seBootMen <- sd(estMedMen)
+
+#Definimos el intervalo de confianza aproximado para la mediana del peso masculino
+alfa <- 0.05
+
+#Z_alfa/2
+z <- qnorm(1-alfa/2)
+
+IDC_Men <- c(T_M - z*seBootMen,T_M + z*seBootMen)
+
+#BootStrap para pesos de Mujeres
+
+# Tomamos 1000 muestras de tamano |WEIGH_M| de los datos que tenemos 
+n <- length(WEIG_W)
+estMedWom<- 0
+
+for(i in 1:B){
+  #Para cada muestra estimamos la mediana y la almacenamos 
+  muestra <- sample(WEIG_W, n, replace = TRUE )
+  estMedWom[i] <- median(muestra)
+}
+
+seBootWom <- sd(estMedWom)
+
+#Definimos el intervalo de confianza aproximado para la mediana del peso masculino
+alfa <- 0.05
+
+#Z_alfa/2
+z <- qnorm(1-alfa/2)
+
+IDC_Wom <- c(T_M - z*seBootWom,T_M + z*seBootWom)
+
+
+
+
+
+
+
